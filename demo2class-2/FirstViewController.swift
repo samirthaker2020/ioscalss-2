ff5626c739dcbc482bb422bd4de697e98bbe409f
MacStudent <macstudent@126-iMac15.local>
2019-03-05 19:57:26 -0500

    new view controller added

--- demo2class-2/FirstViewController.swift
+++ demo2class-2/FirstViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var lblresult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func sw1(_ sender: UISwitch) {
+        var count:Bool=false
+        if sender.isOn
+         {
+            lblresult.text="on"
+            let alert=UIAlertController(title: "You ON The Switch", message: "Do u Want to Make It ON", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "YES", style: .default, handler: {
+                ACTION in
+                sender.setOn(true, animated: true)
+                
+            }))
+            alert.addAction(UIAlertAction(title: "NO", style: .destructive, handler: {
+                ACTION in
+                sender.setOn(false, animated: true)
+            }))
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: {
+                ACTION in
+                sender.setOn(false, animated: true)
+            }))
+            self.present(alert,animated: true)
+        }
+        else
+         {
+            lblresult.text="off"
+        }
+    }
 }
 
